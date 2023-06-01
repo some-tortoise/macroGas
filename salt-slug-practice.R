a75937d809abd075dc319b9c5d713976a3bd8fa6
kaleyann <sperlingkaley@gmail.com>
2023-06-01 14:47:07 -0400

    Updates

--- salt-slug-practice.R
+++ salt-slug-practice.R
@@ -1,34 +1,20 @@
----
-title: "Salt Slug Practice",
-Author: "Kaley Sperling"
----
-
-Cleaning Code
-
-```{r}
-#| library-packages
-
+## ----------------------------------------------------------------------
 library(tidyverse)
 library(googledrive)
 library(googlesheets4)
 library(lubridate)
-```
 
-```{r}
-#| load-in-files
+
+## ----------------------------------------------------------------------
 id = "1oN30VFTWRDMB7jwRNANu47L183L2G25_"
 copy_mgas_erwin_station_1_2023_05_25 <- read.csv(sprintf("https://docs.google.com/uc?id=%s&export=download", id), header = FALSE)
-```
 
-```{r}
-#| view-data
 
+## ----------------------------------------------------------------------
 view(copy_mgas_erwin_station_1_2023_05_25)
-```
 
-```{r}
-#| clean-data-1
 
+## ----------------------------------------------------------------------
 organized_mgas_erwin_station_1_2023_05_25 <-  
 copy_mgas_erwin_station_1_2023_05_25 |>
   slice(2:11805)|>
@@ -36,45 +22,38 @@ copy_mgas_erwin_station_1_2023_05_25 |>
   rename(Date_Time = V2, Low_Range = V3, Full_Range = V4, Temp_C = V5) |>
   slice(-1) |>
   separate(Date_Time, into = c("Date", "Time"), sep = " ")
-```
 
-```{r}
+
+## ----------------------------------------------------------------------
 #|view-cleaned-file
 view(organized_mgas_erwin_station_1_2023_05_25)
-```
 
-```{r}
-#| clean-data-2
 
+## ----------------------------------------------------------------------
 organized_mgas_erwin_station_1_2023_05_25 <- organized_mgas_erwin_station_1_2023_05_25 |>
  mutate(Low_Range = as.numeric(Low_Range)) |>
   mutate(Full_Range = as.numeric(Full_Range)) |>
   mutate(Temp_C = as.numeric(Temp_C))
   
-```
 
-```{r}
-#| changing-date
 
+## ----------------------------------------------------------------------
 organized_mgas_erwin_station_1_2023_05_25$Date <-  as.POSIXct(organized_mgas_erwin_station_1_2023_05_25$Date, format = "%m/%d/%y", tz = "UTC" )
 
 #organized_mgas_erwin_station_1_2023_05_25$Date = #sapply(organized_mgas_erwin_station_1_2023_05_25$Date,)
-```
 
-```{r}
-#| changing-time
-```
 
-Visualizations
+## ----------------------------------------------------------------------
 
-```{r}
+
+## ----------------------------------------------------------------------
 #|making-graph
 select
 organized_mgas_erwin_station_1_2023_05_25[organized_mgas_erwin_station_1_2023_05_25$Date==as.Date('2023-05-25'), ] 
-p1 <- ggplot(organized_mgas_erwin_station_1_2023_05_25[organized_mgas_erwin_station_1_2023_05_25$Date==as.Date('2023-05-25'), ], aes(x = Time, y = Temp_C)) +
+  ggplot(organized_mgas_erwin_station_1_2023_05_25, aes(x = Time, y = Temp_C)) +
   geom_point() +
   geom_line() 
   
-p1
+  
 
-```
+
--- shinySource.R
+++ shinySource.R
@@ -1,10 +1,32 @@
 library(shiny)
+source(
+  knitr::purl("salt-slug-practice.Rmd",
+              output = tempfile(),
+              quiet = TRUE))
 
 ui <- fluidPage(
+    titlePanel("Our App"),
+    
+    sidebarLayout(
+      sidebarPanel("Date",
+                  dateInput("dateInput", 
+                            label = "Write Date Here", 
+                            value = "2023-05-25",
+                            format = "yyyy/mm/dd"),
+                radioButtons("radioInput",
+                                     label = "Y Axis",
+                                     choices = c("Low Range" = "lr", "Full Range" = "fr", "Temp C" = "tempc")),
+                actionButton("run",
+                             label = "run")),
+      
+      mainPanel("Visualization", plotOutput("plotOutput"))
+    
+  )
   
 )
 
 server <- function(input, output){
+  #output$plotOutput <- renderPlot()
   
 }
 
