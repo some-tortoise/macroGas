81b76a22f9adff251ff7d3267951ef583d8dae27
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-01 11:04:10 -0400

    working way to get files from gdocs
    woo hoo.

--- stuff.R
+++ stuff.R
@@ -1,31 +1,27 @@
 library('googledrive')
+library('readr')
+
+drive_auth(
+  email = gargle::gargle_oauth_email(),
+  path = NULL,
+  scopes = "https://www.googleapis.com/auth/drive",
+  cache = gargle::gargle_oauth_cache(),
+  use_oob = gargle::gargle_oob_default(),
+  token = NULL
+)
 
 macro_folder <- 'https://drive.google.com/drive/folders/1QhIzFToANF5JNOyxp2_q6WGaFrFUM3iA'
 folder_id = drive_get(as_id(macro_folder))
-files = drive_ls(folder_id)
-drive_download(files)
-for (i in seq_along(files$name)) {
-  #list files
-  i_dir = drive_ls(files[i, ])
-  
-  #mkdir
-  dir.create(files$name[i])
-  
-  #download files
-  for (file_i in seq_along(i_dir$name)) {
-    #fails if already exists
-    try({
-      drive_download(
-        as_id(i_dir$id[file_i]),
-        path = str_c(files$name[i], "/", i_dir$name[file_i])
-      )
-    })
-  }
-}
+files = drive_ls(folder_id) # gets files from inside folder
+vector_of_CSVs <- c(as.data.frame(a[1]))[[1]] # column 1 tells it to grab first column with names of csv
+num_files <- length(vector_of_CSVs) # number of files in the google drive
+data_list <- list() # list of CSVs in the order they are in google drive.
 
-b <- c(as.data.frame(a[1]))
-b
-drive_download(b[[1]][2], type = "csv")
-out <- read.csv(b[[1]][2], header=0)
-out
+for (x in 1:num_files) {
+  currentCSV <- vector_of_CSVs[x] 
+  drive_download(currentCSV, overwrite = TRUE, type = 'csv') # overwrite = TRUE tells us to overwrite local copies of the files. This is extremely inefficient, but does not work if I don't do it so......
+  foo <- read_csv(currentCSV, skip = 1)
+  data_list[[x]] <- foo
+  print(data_list[[x]])
+}
 
