5d066498c5aafa0c89143a8e1bd8654a540629d6
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-01 11:16:43 -0400

    fixing the letter a

--- stuff.R
+++ stuff.R
@@ -13,7 +13,7 @@ drive_auth(
 macro_folder <- 'https://drive.google.com/drive/folders/1QhIzFToANF5JNOyxp2_q6WGaFrFUM3iA'
 folder_id = drive_get(as_id(macro_folder))
 files = drive_ls(folder_id) # gets files from inside folder
-vector_of_CSVs <- c(as.data.frame(a[1]))[[1]] # column 1 tells it to grab first column with names of csv
+vector_of_CSVs <- c(as.data.frame(files[1]))[[1]] # column 1 tells it to grab first column with names of csv
 num_files <- length(vector_of_CSVs) # number of files in the google drive
 data_list <- list() # list of CSVs in the order they are in google drive.
 
@@ -25,3 +25,4 @@ for (x in 1:num_files) {
   print(data_list[[x]])
 }
 
+data_list[[2]]
