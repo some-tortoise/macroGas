70ed9a1e08fcf4d53cf41cf17bbc106eb9b53aac
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-02 12:59:05 -0400

    sucks

--- shinySource.R
+++ shinySource.R
@@ -15,20 +15,20 @@ ui <- fluidPage(
                      choices = c("Low Range" = "Low_Range", "Full Range" = 'Full_Range', "Temp C" = 'Temp_C'))
     ),
       
-      mainPanel(plotlyOutput("plotOutput"))
+      mainPanel(plotOutput("plotOutput"))
     
   )
   
 )
 
 server <- function(input, output){
-  output$plotOutput <- renderPlotly({
-    p <- ggplot(data = clean_data_list[[4]], mapping = aes(x = Date_Time, y = !!as.name(input$radioInput), color = 'red')) +
-      theme(panel.background = element_rect(fill = 'lightgray'), legend.position = 'None') +
-      geom_point() +
-      geom_line() +
-      labs(x = 'Time', y = input$radioInput)
-    ggplotly(p)
+  #output$plotOutput <- renderPlotly({
+  #  p <- ggplot(data = clean_data_list[[4]], mapping = aes(x = Date_Time, y = !!as.name(input$radioInput), color = 'red')) +
+   #   theme(panel.background = element_rect(fill = 'lightgray'), legend.position = 'None') +
+    #  geom_point() +
+     # geom_line() +
+      #labs(x = 'Time', y = input$radioInput)
+    #ggplotly(p)
   
   output$plotOutput <- renderPlot({
     if(input$station=='All'){
--- stuff.R
+++ stuff.R
@@ -1,7 +1,16 @@
 library('googledrive')
 library('readr')
 library('tidyverse')
-library(lubridate)
+library('lubridate')
+
+#
+# GLOBAL VARS
+#
+macro_folder <- 'https://drive.google.com/drive/u/0/folders/12bxB-gn3DvutNcwmH8CO1er3ngmCjdlo'
+
+#
+# FUNCTIONS
+#
 
 google_drive_auth <- function(){
   drive_auth(
@@ -15,9 +24,8 @@ google_drive_auth <- function(){
 }
 
 download_csv_files <- function(){
-  macro_folder <- 'https://drive.google.com/drive/u/0/folders/1sulvzGLL6dfAUfFTy6NCSDPOlVVNTWu-'
-  folder_id = drive_get(as_id(macro_folder))
-  files = drive_ls(folder_id) # gets files from inside folder
+  folder_id <- drive_get(as_id(macro_folder))
+  files <- drive_ls(folder_id) # gets files from inside folder
   vector_of_CSVs <- c(as.data.frame(files[1]))[[1]] # column 1 tells it to grab first column with names of csv
   num_files <- length(vector_of_CSVs) # number of files in the google drive
   data_list <- list() # list of CSVs in the order they are in google drive.
@@ -25,11 +33,10 @@ download_csv_files <- function(){
   for (x in 1:num_files) {
     currentCSV <- vector_of_CSVs[x] 
     drive_download(currentCSV, overwrite = TRUE, type = 'csv') # overwrite = TRUE tells us to overwrite local copies of the files. This is extremely inefficient, but does not work if I don't do it so......
-    foo <- read_csv(currentCSV, skip = 1)
-    data_list[[x]] <- foo
-    print(data_list[[x]])
+    data_list[[x]] <- read_csv(currentCSV, skip = 1) 
+    print(data_list[[x]]) # just for visuals during the downloading process
   }
-  data_list <- lapply(data_list, as.data.frame)
+  data_list <- lapply(data_list, as.data.frame) # turns the tibble into a dataframes
   return(data_list)
 }
 
@@ -59,12 +66,12 @@ clean_csv_files <- function(df, x){
 
 #google_drive_auth()
 data_list <- download_csv_files() # use data_list[[x]] to get the x'th tibble. Range from 1:num_files
-num_files <- length(data_list)
 
+num_files <- length(data_list)
 clean_data_list <- list()
 for (x in seq(1:num_files)){
   clean_data_list[[x]] <- clean_csv_files(data_list[[x]], x)
 }
 
-combined_df = do.call(rbind, clean_data_list)
+#combined_df = do.call(rbind, clean_data_list)
 
