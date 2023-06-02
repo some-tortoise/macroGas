028660a1b395556b6b2a12389f52fce65c1583ac
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-02 12:29:35 -0400

    interactive-basic

--- shinySource.R
+++ shinySource.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(plotly)
 source(
   knitr::purl("stuff.R",
               output = tempfile(),
@@ -10,23 +11,24 @@ ui <- fluidPage(
     sidebarLayout(
       sidebarPanel("",
                 radioButtons("radioInput",
-                                     label = helpText('Select variable to graph'),
-                                     choices = c("Low Range" = "Low_Range", "Full Range" = 'Full_Range', "Temp C" = 'Temp_C')),
+                             label = helpText('Select variable to graph'),
+                             choices = c("Low Range" = "Low_Range", "Full Range" = 'Full_Range', "Temp C" = 'Temp_C')),
     ),
       
-      mainPanel(plotOutput("plotOutput"))
+      mainPanel(plotlyOutput("plotOutput"))
     
   )
   
 )
 
 server <- function(input, output){
-  output$plotOutput <- renderPlot({
-    ggplot(data = clean_data_list[[4]], mapping = aes(x = Date_Time, y = !!as.name(input$radioInput), color = 'red')) +
+  output$plotOutput <- renderPlotly({
+    p <- ggplot(data = clean_data_list[[4]], mapping = aes(x = Date_Time, y = !!as.name(input$radioInput), color = 'red')) +
       theme(panel.background = element_rect(fill = 'lightgray'), legend.position = 'None') +
       geom_point() +
       geom_line() +
       labs(x = 'Time', y = input$radioInput)
+    ggplotly(p)
   })
 }
 
