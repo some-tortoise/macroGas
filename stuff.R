ebe6e754fe0129ae552e5a4b93a16b5af4fc59bc
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-05 14:23:56 -0400

    microwave go brr

--- stuff.R
+++ stuff.R
@@ -79,10 +79,10 @@ clean_csv_files <- function(dfs){
 }
 
 upload_csv_file <- function(clean_df, name){
-  file <- paste(name,'.csv', sep='')
+  file <- name
   file <- drive_put(
     media = file,
-    name = name,
+    name = paste('processed_',name, sep=''),
     type = 'csv',
     path = as_id(processed_folder))
   print(2)
@@ -92,14 +92,14 @@ turn_file_to_csv <- function(clean_df){
   write.csv(clean_df, "./chicken.csv", row.names=FALSE)
 }
 
-upload_files <- function(clean_dfs){
+upload_files <- function(clean_dfs, list_of_raw_names){
   for(x in seq(1:length(clean_dfs))){
     turn_file_to_csv(clean_dfs[[x]])
-    upload_csv_file(clean_dfs[[x]], 'chicken')
+    upload_csv_file(clean_dfs[[x]], as.character(nth(list_of_raw_names, x)))
   }
 }
 
-google_drive_auth()
+#google_drive_auth()
 download_output <- download_csv_files() # use data_list[[x]] to get the x'th tibble. Range from 1:num_files
 data_list <- download_output[[1]]
 list_of_raw_names <- download_output[[2]]
@@ -108,5 +108,4 @@ clean_data_list <- clean_csv_files(data_list)
 
 combined_df = do.call(rbind, clean_data_list)
 
-upload_files(clean_data_list)
-
+upload_files(clean_data_list, list_of_raw_names)
