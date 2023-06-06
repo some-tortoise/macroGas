911b327d36d9aaa5f57b17ddcd7ef333f42f09bc
K <qw117@duke.edu>
2023-06-06 16:45:14 -0400

    Update ShinyBrush.R
    Add comments on Leon's code

--- ShinyBrush.R
+++ ShinyBrush.R
@@ -1,3 +1,4 @@
+## --------------------------------------------------------------------------------------------
 library(shiny)
 library(plotly)
 library(crosstalk)
@@ -7,25 +8,28 @@ library(shinyTime)
 source(
   knitr::purl('updated_cleaning.R',
               output = tempfile(),
-              quiet = TRUE))
+              quiet = TRUE)) #gets cleaned data
 
+#Creates a user interface
 ui <- fluidPage(
-    titlePanel('Salt Slug Visualizations'),
-    
+    titlePanel('Salt Slug Visualizations'), #names the browser window title
+
+#Designs the side bar
     sidebarLayout(
       sidebarPanel(
-        selectInput('station', label = 'Select station', c(1, 2, 3, 4, 5)),
-        radioButtons('radioInput',label = helpText('Select variable to graph'),
+        selectInput('station', label = 'Select station', c(1, 2, 3, 4, 5)), #5 stations to choose from
+        radioButtons('radioInput',label = helpText('Select variable to graph'), #Used to visualize one of the variables when clicked
                      c('Low Range' = 'Low_Range', 'Full Range' = 'Full_Range', 'Temp C' = 'Temp_C')),
-        actionButton('flag', label = 'flag bad points'),
-        actionButton('reset_flag', label = 'reset the flag for the station'),
+        actionButton('flag', label = 'flag bad points'), #creates a botton to flag points of interests
+        actionButton('reset_flag', label = 'reset the flag for the station'), #creates a button to reset all the flags to be "good"
         hr(),
-        actionButton('final',label = 'show final table'),
-        actionButton('repaint', label = 'repaint the plot'),
-        actionButton('add_salt', label = 'add slug started time')
+        actionButton('final',label = 'show final table'), #creates a button to show the final table
+        actionButton('repaint', label = 'repaint the plot'), #creates a button to replot
+        actionButton('add_salt', label = 'add slug started time') #creates a button to add the time that salt slug is added
         
-    ),
-      
+    ), 
+    
+#Designs the main panel
       mainPanel(plotlyOutput('plotOutput'),
                 dataTableOutput('selectedData'),
                 dataTableOutput('finalDT'),
@@ -35,14 +39,17 @@ ui <- fluidPage(
   
 )
 
+#Creates a R session that runs code and returns results
 server <- function(input, output){
-  
-  data <- reactiveValues(df = combined_df |>
+ 
+  #Adds "Flag" columns to each variables
+  data <- reactiveValues(df = combined_df |> #Creates a global variable to store the original table
                            mutate(Low_Range_Flag = 'good',
                                   Full_Range_Flag = 'good',
-                                  Temp_C_Flag = 'good'),
-                         p = c())
+                                  Temp_C_Flag = 'good'), #Sets the default flags to be "good"
+                         p = c()) #Creates a global variable p to store the plots
   
+  #A function to generate the plot
   observe({
     df_plot = SharedData$new(data$df[data$df$station == as.numeric(input$station),])
     data$p <- ggplot(data = df_plot, aes(x = Date_Time, y = !!as.name(input$radioInput), color = 'red')) +
@@ -52,8 +59,9 @@ server <- function(input, output){
       labs(x = 'Time', y = input$radioInput)
   })
   
+  #An function to make interactive plots
   output$plotOutput <- renderPlotly({
-    highlight(ggplotly(data$p), 'plotly_selected')
+    highlight(ggplotly(data$p), 'plotly_selected') #Enables selection on the plot
   })
   
   selectedData <- reactive({
@@ -62,6 +70,7 @@ server <- function(input, output){
       data$df[data$df$station == as.numeric(input$station),] %>% slice(event$pointNumber + 1)
   })
   
+  #Generates the table of highlighted points
   output$selectedData <- renderDataTable(
     selectedData(),
     options = list(
@@ -69,6 +78,7 @@ server <- function(input, output){
     )
   )
   
+  #Flags bad data
   observeEvent(input$flag,{
     flag_name = paste0(input$radioInput, "_Flag")
     data$df[flag_name] <- ifelse((data$df$id %in% selectedData()[['id']] & 
@@ -76,6 +86,7 @@ server <- function(input, output){
                                  'bad', data$df[[flag_name]])
   })
   
+  #Generates a table with added flags
   observeEvent(input$final,{
     output$finalDT <- renderDataTable(gather(data$df, key='flags', 
                                              value = 'flag_values', 
@@ -84,6 +95,7 @@ server <- function(input, output){
                                       options = list(pageLength = 5))
   })
   
+  #A function to reset all the flag to the default status "good"
   observeEvent(input$reset_flag, {
     data$df <- combined_df |>
       mutate(Low_Range_Flag = 'good',
@@ -91,6 +103,7 @@ server <- function(input, output){
              Temp_C_Flag = 'good')
   })
   
+  #A function to re-graph
   observeEvent(input$repaint, {
     df_plot = SharedData$new(data$df[data$df$station == as.numeric(input$station),])
     data$p <- ggplot(data = df_plot, aes(x = Date_Time, y = !!as.name(input$radioInput), 
@@ -101,8 +114,9 @@ server <- function(input, output){
       labs(x = 'Time', y = input$radioInput)
   })
   
+  #A function to add the time of adding salt slug
   observeEvent(input$add_salt, {
-    showModal(modalDialog(
+    showModal(modalDialog( #Popping up a window to ask user the adding time
       title = "Add time of starting the slug",
       dateInput('slug_date',label='add the date(ymd)',value = "2023-05-25"),
       textInput('slug_time',label='add time(hms)', value = "12:00:00"),
@@ -121,4 +135,6 @@ server <- function(input, output){
 
 }
 
+#Run shiny app
 shinyApp(ui = ui, server = server)
+
