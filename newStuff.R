9afc74276ac309bd80f7cc9f8165e1edbee862cd
kaleyann <sperlingkaley@gmail.com>
2023-06-07 11:31:20 -0400

    stuff?

--- newStuff.R
+++ newStuff.R
@@ -11,7 +11,7 @@ source(
 
 ui <- fluidPage(
   sidebarPanel(
-    selectInput('station', label = 'Select station', c('All',1, 2, 3, 4, 5)),
+    checkboxGroupInput('station', label = 'Select station', c(1, 2, 3, 4, 5)),
     radioButtons("variable_choice",label = helpText('Select variable to graph'),
                  choices = c("Low Range" = "Low_Range", "Full Range" = 'Full_Range', "Temp C" = 'Temp_C')),
     selectInput('flag_type_select', label = 'Select flag type', c('good', 'QuEstionable', 'inTeresting!', 'bAAd')),
@@ -25,17 +25,18 @@ ui <- fluidPage(
 
 server <- function(input, output, session){
   output$myPlot = renderPlotly({
-    df_plot <- combined_df[combined_df$station == as.numeric(input$station),]
-    plot_ly(data = df_plot, x = ~Date_Time, y = as.formula(paste0('~',input$variable_choice)), key = ~id, color = ~Temp_C, source = "imgLink") %>%
+    df_plot <- combined_df[combined_df$station == as.character(input$station),]
+    plot_ly(data = df_plot, x = ~Date_Time, y = as.formula(paste0('~',input$variable_choice)), key = ~id, color = ~station, source = "imgLink") %>%
       layout(xaxis = list(
         range = c(min(df_plot$Date_Time), max(df_plot$Date_Time)),  # Set the desired range
         type = "date"  # Specify the x-axis type as date
-      ), dragmode = 'select')
+      ), dragmode = 'select', 
+      legend = "Station"))
     
   })
   
   output$selectedData <- renderDT({
-    df_plot <- combined_df[combined_df$station == as.numeric(input$station),]
+    df_plot <- combined_df[combined_df$station == as.character(input$station),]
     event.click.data <- event_data(event = "plotly_click", source = "imgLink")
     event.selected.data <- event_data(event = "plotly_selected", source = "imgLink")
     df_chosen <- df_plot[((df_plot$id %in% event.click.data$key) | (df_plot$id %in% event.selected.data$key)),]
@@ -46,7 +47,7 @@ server <- function(input, output, session){
     flag_name = paste0(input$flag_type_select, "_Flag")
     combined_df[flag_name] <- 
     data$df[flag_name] <- ifelse((data$df$id %in% selectedData()[['id']] & 
-                                    data$df$station==as.numeric(input$station)), 
+                                    data$df$station==as.character(input$station)), 
                                  'bad', data$df[[flag_name]])
   })
 }
