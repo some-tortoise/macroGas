c014e712f56ac757fbf7f985f3dd6c022fe4027c
K <qw117@duke.edu>
2023-06-07 07:44:24 -0400

    Update ShinyBrush.R
    added some confirmation dialogs & capitalized some letters in label

--- ShinyBrush.R
+++ ShinyBrush.R
@@ -10,46 +10,47 @@ source(
               output = tempfile(),
               quiet = TRUE)) #gets cleaned data
 
-#Creates a user interface
-ui <- fluidPage(
-    titlePanel('Salt Slug Visualizations'), #names the browser window title
 
-#Designs the side bar
+## --------------------------------------------------------------------------------------------
+# Creating a user interface
+ui <- fluidPage(
+    titlePanel('Salt Slug Visualizations'), # This is the browser window title
+
+    # Designing the side bar
     sidebarLayout(
       sidebarPanel(
-        selectInput('station', label = 'Select station', c(1, 2, 3, 4, 5)), #5 stations to choose from
-        radioButtons('radioInput',label = helpText('Select variable to graph'), #Used to visualize one of the variables when clicked
+        selectInput('station', label = 'Select Station', c(1, 2, 3, 4, 5)), # 5 stations to choose from
+        radioButtons('radioInput',label = helpText('Select Variable to Graph'), # To visualize one of the variables if presssed
                      c('Low Range' = 'Low_Range', 'Full Range' = 'Full_Range', 'Temp C' = 'Temp_C')),
-        actionButton('flag', label = 'flag bad points'), #creates a botton to flag points of interests
-        actionButton('reset_flag', label = 'reset the flag for the station'), #creates a button to reset all the flags to be "good"
+        actionButton('flag', label = 'Flag Bad Points'), #creates a button to flag questionable points
+        actionButton('reset_flag', label = 'Reset the Flags for the Station'), #creates a button to reset all the flags to be "good"
         hr(),
-        actionButton('final',label = 'show final table'), #creates a button to show the final table
-        actionButton('repaint', label = 'repaint the plot'), #creates a button to replot
-        actionButton('add_salt', label = 'add slug started time') #creates a button to add the time that salt slug is added
-        
+        actionButton('final',label = 'Show Final Table'), #creates a button to show the final table
+        actionButton('repaint', label = 'Repaint the Plot'), #creates a button to re-plot
+        actionButton('add_salt', label = 'Add Slug Started Time') #creates a button to add the time that the salt slug was added
     ), 
     
-#Designs the main panel
+      # Designing the main panel
       mainPanel(plotlyOutput('plotOutput'),
                 dataTableOutput('selectedData'),
                 dataTableOutput('finalDT'),
                 textOutput("date"))
-    
   )
-  
 )
 
-#Creates a R session that runs code and returns results
+
+## --------------------------------------------------------------------------------------------
+# Creating a R session that runs code and returns results
 server <- function(input, output){
  
-  #Adds "Flag" columns to each variables
-  data <- reactiveValues(df = combined_df |> #Creates a global variable to store the original table
+  # Adding "Flag" columns to each variables
+  data <- reactiveValues(df = combined_df |> #Creates a global variable 'df' to store the original table
                            mutate(Low_Range_Flag = 'good',
                                   Full_Range_Flag = 'good',
                                   Temp_C_Flag = 'good'), #Sets the default flags to be "good"
-                         p = c()) #Creates a global variable p to store the plots
+                         p = c()) #Creates a global variable 'p' to store the plots
   
-  #A function to generate the plot
+  # A function to generate the plot
   observe({
     df_plot = SharedData$new(data$df[data$df$station == as.numeric(input$station),])
     data$p <- ggplot(data = df_plot, aes(x = Date_Time, y = !!as.name(input$radioInput), color = 'red')) +
@@ -59,7 +60,7 @@ server <- function(input, output){
       labs(x = 'Time', y = input$radioInput)
   })
   
-  #An function to make interactive plots
+  # A function to make interactive plots
   output$plotOutput <- renderPlotly({
     highlight(ggplotly(data$p), 'plotly_selected') #Enables selection on the plot
   })
@@ -70,7 +71,7 @@ server <- function(input, output){
       data$df[data$df$station == as.numeric(input$station),] %>% slice(event$pointNumber + 1)
   })
   
-  #Generates the table of highlighted points
+  # Generates the table of highlighted points
   output$selectedData <- renderDataTable(
     selectedData(),
     options = list(
@@ -78,15 +79,32 @@ server <- function(input, output){
     )
   )
   
-  #Flags bad data
-  observeEvent(input$flag,{
-    flag_name = paste0(input$radioInput, "_Flag")
-    data$df[flag_name] <- ifelse((data$df$id %in% selectedData()[['id']] & 
-                                    data$df$station==as.numeric(input$station)), 
-                                 'bad', data$df[[flag_name]])
-  })
-  
-  #Generates a table with added flags
+  # Flags bad data
+  observeEvent(input$flag, {
+  # Display a confirmation dialog
+  showModal(
+    modalDialog(
+      title = "Flag Data",
+      "Are you sure you want to flag those data?",
+      footer = tagList(
+        actionButton("confirm_flag", "Confirm", class = "btn-primary"),
+        modalButton("Cancel")
+      )
+    )
+  )
+})
+
+observeEvent(input$confirm_flag, {
+  # Flag the data if confirmed
+  flag_name <- paste0(input$radioInput, "_Flag")
+  data$df[flag_name] <- ifelse((data$df$id %in% selectedData()[['id']] &
+                                 data$df$station == as.numeric(input$station)),
+                              'bad', data$df[[flag_name]])
+  removeModal()
+  shinyalert::alert("Data Flagged", type = "success")
+})
+
+  # Generates a table with flags
   observeEvent(input$final,{
     output$finalDT <- renderDataTable(gather(data$df, key='flags', 
                                              value = 'flag_values', 
@@ -95,14 +113,34 @@ server <- function(input, output){
                                       options = list(pageLength = 5))
   })
   
-  #A function to reset all the flag to the default status "good"
+  # A function to reset all the flags
   observeEvent(input$reset_flag, {
-    data$df <- combined_df |>
-      mutate(Low_Range_Flag = 'good',
-             Full_Range_Flag = 'good',
-             Temp_C_Flag = 'good')
-  })
-  
+  # Display a confirmation dialog
+  showModal(
+    modalDialog(
+      title = "Reset Flags",
+      "Are you sure you want to reset all the flags?",
+      footer = tagList(
+        actionButton("confirm_reset", "Confirm", class = "btn-primary"),
+        modalButton("Cancel")
+      )
+    )
+  )
+})
+
+observeEvent(input$confirm_reset, {
+  # Reset the flags if confirmed
+  data$df <- combined_df |>
+    mutate(
+      Low_Range_Flag = 'good',
+      Full_Range_Flag = 'good',
+      Temp_C_Flag = 'good'
+    )
+  removeModal()
+  shinyalert::alert("Flags Reset", type = "success")
+})
+
+
   #A function to re-graph
   observeEvent(input$repaint, {
     df_plot = SharedData$new(data$df[data$df$station == as.numeric(input$station),])
@@ -114,13 +152,13 @@ server <- function(input, output){
       labs(x = 'Time', y = input$radioInput)
   })
   
-  #A function to add the time of adding salt slug
+  #A function to input the salt slug adding time
   observeEvent(input$add_salt, {
-    showModal(modalDialog( #Popping up a window to ask user the adding time
-      title = "Add time of starting the slug",
-      dateInput('slug_date',label='add the date(ymd)',value = "2023-05-25"),
-      textInput('slug_time',label='add time(hms)', value = "12:00:00"),
-      actionButton("Apply","Apply change", icon = NULL, width = NULL),
+    showModal(modalDialog( #Popping up a window to ask user to input date & time
+      title = "Input Date and Time of Adding the Salt Slug",
+      dateInput('slug_date',label='Add date (ymd)',value = "2023-05-25"),
+      textInput('slug_time',label='Add time (hms)', value = "12:00:00"),
+      actionButton("Apply","Apply Change", icon = NULL, width = NULL),
       easyClose = FALSE,
       footer = tagList(
         modalButton("Close")
@@ -135,6 +173,22 @@ server <- function(input, output){
 
 }
 
+
+## --------------------------------------------------------------------------------------------
 #Run shiny app
 shinyApp(ui = ui, server = server)
+
+
+## --------------------------------------------------------------------------------------------
+library(knitr)
+
+# Set the path to your .Rmd file
+rmd_file <- "~/Desktop/r4ds/ShinyBrush.Rmd"
+
+# Specify the output file path for the .R script
+r_script <- "~/Desktop/r4ds/ShinyBrush.R"
+
+# Use purl() to convert .Rmd to .R script
+purl(rmd_file, output = r_script)
+
 
