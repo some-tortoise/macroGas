e99b33602d1f6098ecf2480c8d61f1e8d67d12f4
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-07 11:31:33 -0400

    eggs

--- newStuff.R
+++ newStuff.R
@@ -14,18 +14,29 @@ ui <- fluidPage(
     selectInput('station', label = 'Select station', c('All',1, 2, 3, 4, 5)),
     radioButtons("variable_choice",label = helpText('Select variable to graph'),
                  choices = c("Low Range" = "Low_Range", "Full Range" = 'Full_Range', "Temp C" = 'Temp_C')),
-    selectInput('flag_type_select', label = 'Select flag type', c('good', 'QuEstionable', 'inTeresting!', 'bAAd')),
+    selectInput('flag_type', label = 'Select flag type', c('good', 'QuEstionable', 'inTeresting!', 'bAd')),
     actionButton('flag_btn', label = 'flag points')
   ),
   mainPanel(
-    plotlyOutput('myPlot'),
-    dataTableOutput('selectedData')
+    plotlyOutput('main_plot'),
+    dataTableOutput('selected_data_table')
   )
 )
 
 server <- function(input, output, session){
-  output$myPlot = renderPlotly({
+  
+  data <- reactiveValues(df = combined_df)
+  
+  selectedData <- reactive({
     df_plot <- combined_df[combined_df$station == as.numeric(input$station),]
+    event.click.data <- event_data(event = "plotly_click", source = "imgLink")
+    event.selected.data <- event_data(event = "plotly_selected", source = "imgLink")
+    df_chosen <- df_plot[((df_plot$id %in% event.click.data$key) | (df_plot$id %in% event.selected.data$key)),]
+    return(df_chosen)
+  })
+  
+  output$main_plot = renderPlotly({
+    df_plot <- data$df[data$df$station == as.numeric(input$station),]
     plot_ly(data = df_plot, x = ~Date_Time, y = as.formula(paste0('~',input$variable_choice)), key = ~id, color = ~Temp_C, source = "imgLink") %>%
       layout(xaxis = list(
         range = c(min(df_plot$Date_Time), max(df_plot$Date_Time)),  # Set the desired range
@@ -34,8 +45,8 @@ server <- function(input, output, session){
     
   })
   
-  output$selectedData <- renderDT({
-    df_plot <- combined_df[combined_df$station == as.numeric(input$station),]
+  output$selected_data_table <- renderDT({
+    df_plot <- data$df[data$df$station == as.numeric(input$station),]
     event.click.data <- event_data(event = "plotly_click", source = "imgLink")
     event.selected.data <- event_data(event = "plotly_selected", source = "imgLink")
     df_chosen <- df_plot[((df_plot$id %in% event.click.data$key) | (df_plot$id %in% event.selected.data$key)),]
@@ -43,12 +54,10 @@ server <- function(input, output, session){
   })
   
   observeEvent(input$flag_btn,{
-    flag_name = paste0(input$flag_type_select, "_Flag")
-    combined_df[flag_name] <- 
-    data$df[flag_name] <- ifelse((data$df$id %in% selectedData()[['id']] & 
-                                    data$df$station==as.numeric(input$station)), 
-                                 'bad', data$df[[flag_name]])
+    flag_name = paste0(input$variable_choice, "_Flag")
+    data$df[data$df$id %in% selectedData()$id,flag_name] <- input$flag_type
   })
+  
 }
 
 shinyApp(ui, server)
