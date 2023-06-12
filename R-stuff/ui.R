8c9c551b25c3a72c98c0caef1d18adda88916d57
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-12 14:15:20 -0400

    er

--- R-stuff/ui.R
+++ R-stuff/ui.R
@@ -8,7 +8,6 @@ library(shinyTime)
 source(knitr::purl("../updated_cleaning.R", output = tempfile(), quiet = TRUE)) #gets cleaned data
 
 ui <- navbarPage(
-  strong("Salt Slugs"),
   tags$head(
     tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
     tags$link(rel = "preconnect", href = "https://fonts.googleapis.com"),
@@ -26,23 +25,31 @@ ui <- navbarPage(
                 h1("Salt Slug App"),
                p("Placeholder text welcoming science people to the salt slug visualization/computation app")
             ),
-  {tabPanel('Home',
-                      titlePanel(strong("Salt Slugs")),
-                      p( "Placeholder text welcoming science people to the salt slug visualization/computation app"),
                       br(),
                       br(),
-                      hr(),
-                      h4(strong("Resources:")),
-                      p("For more information:",
-                        tags$a(href = "http://dx.doi.org/10.1029/2011WR010942", "Covino et al. 2011. Stream-groundwater exchange and hydrologic turnover at the network scale.'")),
-                      br(),
-                      p('Template for salt slug data upload'),
-                      p('Example salt slug data'),
-                      div(id = 'upload_method',
-                          tags$h1('How do you want to upload?'),
-                          actionButton('gdrive_choice','Through Google Drive'),
-                          actionButton('manual_choice','Manually')
-                      )
+            div(id = "box_2",
+                style = " background-color: #FCF7E5 ;
+                width: 800px;
+                height: 200px;
+                padding: 10px;
+                float: right",
+                h2("Instructions and Resouces"),
+                p("For more information:",
+                  tags$a(href = "http://dx.doi.org/10.1029/2011WR010942", "Covino et al. 2011. Stream-groundwater exchange and hydrologic turnover at the network scale.'")),
+             p('Template for salt slug data upload'),
+              p('Example salt slug data')),
+            br(),
+            br(),
+          
+              div(id = 'upload_method',
+                  style = "
+width: 800px;
+                height: 200px;
+                padding: 10px",
+                   tags$h3('How do you want to upload?'),
+                   actionButton('gdrive_choice','Through Google Drive'),
+                   actionButton('manual_choice','Manually')
+                      ))
                       
                       
              )},#HOME
