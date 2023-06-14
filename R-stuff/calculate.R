7ae1b8d4e3169d4bcc50d691cd9b71b59212d7a0
kaleyann <sperlingkaley@gmail.com>
2023-06-14 15:08:49 -0400

    were done???

--- R-stuff/calculate.R
+++ R-stuff/calculate.R
@@ -9,7 +9,12 @@ library(shinyTime)
  div(
  column(width= 5,
         selectInput("station_picker", label = "Choose A Station", c(1, 2, 3, 4, 5)),
-      numericInput("salt_mass", label = "Enter NaCl Mass Here", value = 1)
+      numericInput("salt_mass", label = "Enter NaCl Mass Here", value = 1),
+      textOutput("dischargeOutput", "Discharge")
 
-  )
+  ),
+ column(width = 7,
+        plotOutput("dischargecalcplot"),
+        dataTableOutput("dischargetable"))
 )
+ 
