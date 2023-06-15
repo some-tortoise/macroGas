c8360ef35e79fa0ceb1ab3e3d531f9cfb34e997d
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-15 11:08:12 -0400

    w

--- R-stuff/visualize.R
+++ R-stuff/visualize.R
@@ -6,16 +6,16 @@ library(shinyjs)
 library(shinyFiles)
 library(shinyTime)
 
-div(class = 'visualize-panel-container panel-container',
-  div(
-    column(width = 5,
-                 checkboxGroupInput('station', label = 'Select station', c(1, 2, 3, 4, 5)),
-                 radioButtons("variable_choice",label = helpText('Select variable to graph'),
-                              choices = c("Low Range" = "Low_Range", "Full Range" = 'Full_Range', "Temp C" = 'Temp_C'))
-                 ),
-    column(width= 7,
-              plotlyOutput('main_plot'),
-              dataTableOutput('selected_data_table')
-    )
-  )
-)
+ div(class = 'visualize-panel-container panel-container'
+#   div(
+#     column(width = 5,
+#                  checkboxGroupInput('station', label = 'Select station', c(1, 2, 3, 4, 5)),
+#                  radioButtons("variable_choice",label = helpText('Select variable to graph'),
+#                               choices = c("Low Range" = "Low_Range", "Full Range" = 'Full_Range', "Temp C" = 'Temp_C'))
+#                  ),
+#     column(width= 7,
+#               plotlyOutput('main_plot'),
+#               dataTableOutput('selected_data_table')
+#     )
+#   )
+ )
--- R-stuff/www/script.js
+++ R-stuff/www/script.js
@@ -19,36 +19,72 @@ function openTab(evt, name) {
 
 
     tabcontent = document.getElementsByClassName("panel-container");
+    var content_div = document.getElementsByClassName("tabbable")[0];
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+      tabcontent[i].style.display = "block";
       if(tabcontent[i].classList.contains('home-panel-container') && evt.currentTarget.parentElement.classList.contains('home-nav-el')){
         tabcontent[i].style.display = "block";
+        content_div.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
       }
       if(tabcontent[i].classList.contains('upload-panel-container') && evt.currentTarget.parentElement.classList.contains('upload-nav-el')){
         tabcontent[i].style.display = "block";
+        content_div.scrollTo({
+            top: 740,
+            behavior: 'smooth'
+        });
       }
       if(tabcontent[i].classList.contains('flag-panel-container') && evt.currentTarget.parentElement.classList.contains('flag-nav-el')){
         tabcontent[i].style.display = "block";
+        content_div.scrollTo({
+            top: 1520,
+            behavior: 'smooth'
+        });
       }
       if(tabcontent[i].classList.contains('calculate-panel-container') && evt.currentTarget.parentElement.classList.contains('calculate-nav-el')){
         tabcontent[i].style.display = "block";
+        content_div.scrollTo({
+            top: 2277,
+            behavior: 'smooth'
+        });
       }
-      if(tabcontent[i].classList.contains('visualization-panel-container') && evt.currentTarget.parentElement.classList.contains('visualization-nav-el')){
+      if(tabcontent[i].classList.contains('visualize-panel-container') && evt.currentTarget.parentElement.classList.contains('visualize-nav-el')){
         tabcontent[i].style.display = "block";
+        content_div.scrollTo({
+            top: 3032,
+            behavior: 'smooth'
+        });
       }
-    }
-    /*var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";*/
+    }  
 }
 
-// Get the element with id="defaultOpen" and click on it
-//document.getElementById("defaultOpen").click();
+document.getElementsByClassName("tabbable")[0].addEventListener("scroll", scrollOpenTab);
+
+function scrollOpenTab() {
+  //document.getElementById("demo").innerHTML = "You scrolled in div.";
+  var x = document.getElementsByClassName("tabbable")[0].scrollTop;
+  var nav_show = '';
+  if(x >= 0 && x < 740){
+    nav_show = 'home-nav-el';
+  }else if (x >= 740 && x < 1520) {
+    nav_show = 'upload-nav-el';
+  } else if(x >= 1520 && x < 2277){
+    nav_show = 'flag-nav-el';
+  }else if(x >= 2277 && x < 3032){
+    nav_show = 'calculate-nav-el';
+  }else if(x >= 3032){
+    nav_show = 'visualize-nav-el';
+  } 
+  tablinks = document.getElementsByClassName("nav-el-circle");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active-nav-el", "");
+  }
+  tablinks = document.getElementsByClassName("nav-text");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active-nav-text", "");
+  }
+  document.getElementsByClassName(nav_show)[0].children[0].className += " active-nav-el";
+  document.getElementsByClassName(nav_show)[0].children[1].className += " active-nav-text";
+}
