0fb2d63974e8849870857ff06d97b2565ef1b29b
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-15 10:17:14 -0400

    f

--- R-stuff/calculate.R
+++ R-stuff/calculate.R
@@ -7,16 +7,16 @@ div(class = 'calculate-panel-container panel-container',
           column(width = 5,
                  class = 'calculate-input-container',
                  selectInput("station_picker", label = "Choose A Station", c(1, 2, 3, 4, 5)),
-                 numericInput("salt_mass", label = "Enter NaCl Mass Here", value = 1),
-                 textOutput("dischargeOutput")
+                 numericInput("salt_mass", label = "Enter NaCl Mass Here", value = 1)
+                 #textOutput("dischargeOutput")
                  ),
           column(width = 5,
-                 class = 'calculate-graph-container',
-                 plotOutput("dischargecalcplot"),
+                 class = 'calculate-graph-container'
+                 #plotOutput("dischargecalcplot"),
                  )
           ),
-    fluidRow(class = 'calculate-output-dt-container',
-             dataTableOutput("dischargetable")
+    fluidRow(class = 'calculate-output-dt-container'#,
+             #dataTableOutput("dischargetable")
              )
     
 )
