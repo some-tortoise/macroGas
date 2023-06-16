d7d81dedae00a061ff3e5708509ef2239fcef21f
kaleyann <sperlingkaley@gmail.com>
2023-06-16 14:02:15 -0400

    nfjsdfns

--- R-stuff/server/orderserver.R
+++ R-stuff/server/orderserver.R
@@ -4,8 +4,19 @@ library(sortable)
 
 orderserver <- function(input, output, session) {
   
-  observeEvent(input$order, {
-    ordered_labels <- c("Label 1", "Label 2", "Label 3")[input$order]
-    updateTextInput(session, "input1", value = ordered_labels)
-  })
+rank_list_swap <- rank_list(
+    text = "Reorder your stations, earliest to latest",
+    labels = c("Station A", "Station B", "Station C", "Station D", "Station E"),
+    input_id = "rank_list_swap",
+    options = sortable_options(swap = TRUE))
+  
+output$results_swap <- renderPrint(
+  input$rank_list_swap) # This matches the input_id of the rank list
+observe(
+  update_rank_list(
+    "rank_list_swap",
+      session = session
+      )
+    ) %>%
+      bindEvent(input$station_reorder)
 }
--- R-stuff/ui/order.R
+++ R-stuff/ui/order.R
@@ -1,26 +1,17 @@
 library(shiny)
 library(plotly) # for interactive graphs
-library(DT) # for datatables
-library(htmlwidgets)
-library(shinyjs)
-library(shinyFiles)
-library(shinyTime)
 library(sortable)
 
+rank_list_swap <- rank_list(
+  text = "Reorder your stations, earliest to latest",
+  labels = c("Station A", "Station B", "Station C", "Station D", "Station E"),
+  input_id = "rank_list_swap",
+  options = sortable_options(swap = TRUE))
 
 
-div(#class = 'order-panel-container panel-container',
-    column(width = 5,
-       rank_list(
-         id = "sortable",
-         items = c("Label 1", "Label 2", "Label 3"),
-         inputId = "order"
-                )
-          ),
-   column(
-         width = 4,
-         textInput("input1", "Assigned Value")
-          )
-          
-     )
+div(class = 'order-panel-container panel-container',
+column(width = 5,
+      rank_list_swap,
+      actionButton("station_reorder", label = "Submit Station Reorder"))
+)
   
