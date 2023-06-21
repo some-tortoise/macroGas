91f3c0a38b7b75a2ded70c73989845efa3ee5553
kaleyann <sperlingkaley@gmail.com>
2023-06-21 14:22:36 -0400

    Revert "269 girls wanna drink wine"
    This reverts commit 523787e3b74c94d46218f96e5dfefb8560e33083.

--- R-stuff/server/flagserver.R
+++ R-stuff/server/flagserver.R
@@ -36,8 +36,7 @@ source(knitr::purl("../updated_cleaning.R", output = tempfile(), quiet = TRUE))
         range = c(min_date, max_date),  # Set the desired range
         type = "date"  # Specify the x-axis type as date
       ), dragmode = 'select') |>
-      config(modeBarButtonsToRemove = list( "pan2d", "hoverCompareCartesian", "lasso2d", "autoscale", "hoverClosestCartesian")) |> # Remove specific buttons
-      layout(plot_bgcolor='white')
+      config(modeBarButtonsToRemove = list( "pan2d", "hoverCompareCartesian", "lasso2d", "autoscale", "hoverClosestCartesian"))  # Remove specific buttons
     
   })
   
