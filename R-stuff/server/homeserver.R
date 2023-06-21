df36da75335d7d66dbe592d3ef2544a3ff781cee
kaleyann <sperlingkaley@gmail.com>
2023-06-21 14:27:24 -0400

    country roadddsss, take me homeee, to the page

--- R-stuff/server/homeserver.R
+++ R-stuff/server/homeserver.R
@@ -4,19 +4,6 @@ library(DT) # for datatables
 library(shinyjs)
 source(knitr::purl("../updated_cleaning.R", output = tempfile(), quiet = TRUE)) #gets cleaned data
 
-# LOAD IN METHOD CHOICE
 
-  output$downloadFile <- downloadHandler(
-    filename = "saltslug_exampledata.csv",
-    content = function(file) {
-      file_path <- "saltslug_exampledata.csv"  
-      file.copy(file_path, file)})
-  
-  
-  observeEvent(input$manual_choice, {
-  })
-  
-  observeEvent(input$gdrive_choice, {
-    goop$val1 <- paste0('Hello',goop$val1)
-  })
+
 
