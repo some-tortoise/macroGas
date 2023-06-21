d6ec6cdd5bcd1f24c73d35f01cd7f2d142005db6
kaleyann <sperlingkaley@gmail.com>
2023-06-21 09:53:48 -0400

    changes by david bowie

--- R-stuff/server/orderserver.R
+++ R-stuff/server/orderserver.R
@@ -5,11 +5,13 @@ library(shinyjs)
 library(sortable)
 source(knitr::purl("../updated_cleaning.R", output = tempfile(), quiet = TRUE))
 
-color_mapping <- c("1" = "red", "2" = "blue", "3" = "green", "4" = "purple", "5" = "black")
+order_data <- reactiveValues(df = combined_df)
+
+color_mapping <- c("1" = "red", "2" = "orange", "3" = "green", "4" = "blue", "5" = "purple")
 
 #orderserver <- function(input, output, session) {
   output$orig_plot <- renderPlot({
-    ggplot(combined_df, aes(x = Date_Time, y = Full_Range, color = station)) +
+    ggplot(order_data$df, aes(x = Date_Time, y = Full_Range, color = station)) +
       geom_line() +
       scale_color_manual(values = color_mapping)
   })
@@ -17,25 +19,21 @@ color_mapping <- c("1" = "red", "2" = "blue", "3" = "green", "4" = "purple", "5"
   observeEvent(input$station_reorder, {
     ordered_labels <- unlist(input[["rank_list"]])
     ordered_values <- c("1", "2", "3", "4", "5")
-    ordered_values <- ordered_values[match(ordered_labels, c("Station 1", "Station 2", "Station 3", "Station 4", "Station 5"))]
+    ordered_values <- ordered_values[match(ordered_labels, c("red", "orange", "green", "blue", "purple"))]
   
     curNum <- 0
     curVal <- 0
     
-  for (i in 1:length(combined_df$station)) {
-    if(combined_df[i, 'station'] != curVal){
-      curVal <- combined_df[i, 'station']
+  for (i in 1:length(order_data$df$station)) {
+    if(order_data$df[i, 'station'] != curVal){
+      curVal <- order_data$df[i, 'station']
       curNum <- curNum + 1
     }
     
-    combined_df[i, 'station'] <- ordered_values[curNum]
+    order_data$df[i, 'station'] <- ordered_values[curNum]
       
-      }
-  output$ordered_plot <- renderPlot({
-      ggplot(combined_df, aes(x = Date_Time, y = Full_Range, color = station)) +
-              geom_line() +
-      scale_color_manual(values = color_mapping)
-             })
+  }
+  print("Reorder Registered!")  
     })
 #} 
 
