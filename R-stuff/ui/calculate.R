def0092ab82d70a2d7a948ca4f14999382cf367c
annaspitzer <anna.spitzer@duke.edu>
2023-06-23 14:26:01 -0400

    floyd ✅ mayweather ✅✅

--- R-stuff/ui/calculate.R
+++ R-stuff/ui/calculate.R
@@ -1,12 +1,16 @@
 library(shiny) # for webpage creation
 library(plotly) # for interactive graphs
 library(DT) # for datatables
+library(shinyTime)
 
 div(class = 'calculate-panel-container panel-container',
     fluidRow(class = 'calculate-top-container',
           column(width = 5,
                  class = 'calculate-input-container',
                  selectInput("calc_station_picker", label = "Choose A Station", c(1, 2, 3, 4, 5)),
+                 uiOutput("start_time"),
+                 uiOutput("end_time"),
+                 numericInput("background", label = "Enter background conductivity here", value = 100),
                  numericInput("salt_mass", label = "Enter NaCl Mass Here", value = 1),
                  textOutput("dischargeOutput")
                  ),
