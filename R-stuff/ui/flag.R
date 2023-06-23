f583bd9083dafbf0b692cc0136d90c1b8bdfc436
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-23 11:58:10 -0400

    make column widths a bit better on flag

--- R-stuff/ui/flag.R
+++ R-stuff/ui/flag.R
@@ -12,7 +12,7 @@ tags$style(
 )
 div(class = 'flag-panel-container panel-container',
     fluidRow(
-      column(width= 5,
+      column(width= 3,
              radioButtons('station', label = 'Select station to view', c(1, 2, 3, 4, 5)),
              radioButtons("variable_choice",label = helpText('Select variable to graph'),
                           choices = c("Low Range, µs/cm" = "Low_Range", "Full Range, µs/cm" = 'Full_Range', "Temp, C" = 'Temp_C')),
@@ -22,7 +22,7 @@ div(class = 'flag-panel-container panel-container',
              actionButton('flag_btn', label = 'Flag points'),
              actionButton('download', label = 'Download the flagged dataset')
       ),
-      column(width= 7,
+      column(width= 8,
              plotlyOutput('main_plot'),
              dataTableOutput('selected_data_table')
       )
