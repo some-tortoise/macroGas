c15787fdaaef0d27692b4932701554abf96aa086
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-23 15:28:53 -0400

    ui change

--- R-stuff/ui/calculate.R
+++ R-stuff/ui/calculate.R
@@ -1,23 +1,22 @@
-library(shiny) # for webpage creation
-library(plotly) # for interactive graphs
-library(DT) # for datatables
-
 div(class = 'calculate-panel-container panel-container',
     fluidRow(class = 'calculate-top-container',
-          column(width = 5,
-                 class = 'calculate-input-container',
-                 selectInput("calc_station_picker", label = "Choose A Station", c(1, 2, 3, 4, 5)),
-                 numericInput("salt_mass", label = "Enter NaCl Mass Here", value = 1),
-                 textOutput("dischargeOutput")
-                 ),
-          column(width = 5,
-                 class = 'calculate-graph-container',
-                 plotlyOutput("dischargecalcplot"),
-                 )
-          ),
+             column(width = 5,
+                    class = 'calculate-input-container',
+                    selectInput("calc_station_picker", label = "Choose A Station", c(1, 2, 3, 4, 5)),
+                    uiOutput("start_time"),
+                    uiOutput("end_time"),
+                    numericInput("background", label = "Enter background conductivity here", value = 100),
+                    numericInput("salt_mass", label = "Enter NaCl Mass Here", value = 1),
+                    textOutput("dischargeOutput")
+             ),
+             column(width = 5,
+                    class = 'calculate-graph-container',
+                    plotlyOutput("dischargecalcplot"),
+             )
+    ),
     fluidRow(class = 'calculate-output-dt-container',
              dataTableOutput("dischargetable")
-             )
+    )
     
 )
 
