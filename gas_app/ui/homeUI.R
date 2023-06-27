d330a8ed1e41c2349adf2cfdd35ba5263fea9eeb
kaleyann <sperlingkaley@gmail.com>
2023-06-27 11:58:36 -0400

    im hungry

---
+++ gas_app/ui/homeUI.R
@@ -0,0 +1,24 @@
+div(class = 'home-panel-container panel-container',
+    div(
+      id = "box_1",
+      style = "background-color: white ;
+                width: 800px;
+                height: 200px;
+                padding: 10px;",
+      h1("Gas App"),
+      p("Placeholder text welcoming science people to the visualization/computation app")
+    ),
+    br(),
+    br(),
+    div(id = "box_2",
+        style = " background-color: white ;
+                width: 800px;
+                height: 200px;
+                padding: 10px;
+                float: right",
+        h2("Instructions and Resources"),
+        p("For more information:")
+    ),
+    br(),
+    br()
+)
