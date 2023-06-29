8fe609c4ab3998a55cfb6bad26b68c12c86b8b44
Alejandro Breen Herrera <31518701+some-tortoise@users.noreply.github.com>
2023-06-29 11:17:15 -0400

    commit 100 I think

--- salt_slug_app/R-stuff/app.R
+++ salt_slug_app/R-stuff/app.R
@@ -21,10 +21,7 @@ reactlog_enable()
 ui <- fluidPage(
   theme = shinytheme("flatly"),
   useShinyjs(),
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
-  ),
-  navbarPage('SLUGS', id = "navbar",
+  navbarPage(img(src = 'macrogas-logo.png', width = '60px'),id = "navbar",
                  tabPanel('Home',
                           source("ui/home.R")[1]),
                  tabPanel('Upload',
@@ -35,6 +32,9 @@ ui <- fluidPage(
                  tabPanel('Calculate',
                           source("ui/calculate.R")[1])
              ),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
   includeScript(path = "www/script.js")
   )
 
