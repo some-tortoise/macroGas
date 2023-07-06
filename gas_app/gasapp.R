f43eae32520b21a7d9dd0cb016b689dc1bba0863
kaleyann <sperlingkaley@gmail.com>
2023-07-06 09:59:01 -0400

    google drive get works

--- gas_app/gasapp.R
+++ gas_app/gasapp.R
@@ -13,7 +13,33 @@ library(readr)
 library(shinyBS)
 library(shinythemes)
 
-#combined_df <- get from google drive stuff
+
+raw_folder <- 'https://drive.google.com/drive/u/0/folders/1hniqK4ouIs3mFC8utRoiRfWgk1Ct-m9k'
+list_of_raw_csv_names = drive_ls(raw_folder)[['name']] #gives us file names from google drive
+
+##at this point -- select 1 when prompted to give tidyverse API access to your google drive##
+##then run the rest of the code 
+
+get_and_clean_data <- function(){
+  df = c() #creates an empty vector
+  for(i in 1:length(list_of_raw_csv_names)){ #for each name in however many files we have, do the following:
+    name <- list_of_raw_csv_names[i] #gets first name
+    file <- drive_get(name)[1,] #if there is a csv by this name, get it.
+    drive_download(file, path = name, overwrite = TRUE) # downloads a particular file
+    loaded = read.csv(name, header=F) #loads file into r environment
+    loaded = loaded[-c(1),1:3] #deleting first two columns and then keeping remaining 5
+    colnames(loaded) <- c('Date_Time', 'DO_conc', 'Temp_C') #naming columns
+    loaded = loaded %>% #saves following code as loaded
+      mutate_at(vars(-Date_Time), as.numeric) %>% #changes every variable but date_time to numeric
+      mutate(Date_Time = mdy_hms(Date_Time, tz='GMT')) #changes date_time to a mdy_hms format in gmt time zone
+    df[[i]] = loaded #makes the ith element of the list equal to the data frame.
+  }
+  return(df)
+}
+
+clean_dataframe_list <- get_and_clean_data()
+combined_df <- do.call(rbind, clean_dataframe_list)
+
 ui <- fluidPage(
   navbarPage('Gas App',
               theme = shinytheme("flatly"),
