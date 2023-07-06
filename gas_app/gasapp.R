5d9173c46225d6bfa997115ac6c887809ce460dc
kaleyann <sperlingkaley@gmail.com>
2023-07-06 10:30:31 -0400

    gas app is coming along

--- gas_app/gasapp.R
+++ gas_app/gasapp.R
@@ -26,15 +26,19 @@ get_and_clean_data <- function(){
     name <- list_of_raw_csv_names[i] #gets first name
     file <- drive_get(name)[1,] #if there is a csv by this name, get it.
     drive_download(file, path = name, overwrite = TRUE) # downloads a particular file
-    loaded = read.csv(name, header=F) #loads file into r environment
-    loaded = loaded[-c(1),1:3] #deleting first two columns and then keeping remaining 5
+    loaded = read.csv(name, header = FALSE) #loads file into r environment
+    loaded = loaded[-1] #deleting first column
+    loaded = loaded[1:3] #keeping first 3 columns
     colnames(loaded) <- c('Date_Time', 'DO_conc', 'Temp_C') #naming columns
+    loaded <- slice(loaded, -(1:2))
+    view(loaded)
     loaded = loaded %>% #saves following code as loaded
       mutate_at(vars(-Date_Time), as.numeric) %>% #changes every variable but date_time to numeric
-      mutate(Date_Time = mdy_hms(Date_Time, tz='GMT')) #changes date_time to a mdy_hms format in gmt time zone
+      mutate(Date_Time = as.POSIXct(Date_Time, format = "%m/%d/%y %I:%M:%S %p")) 
     df[[i]] = loaded #makes the ith element of the list equal to the data frame.
   }
   return(df)
+  view()
 }
 
 clean_dataframe_list <- get_and_clean_data()
--- gas_app/server/qaqcserver.R
+++ gas_app/server/qaqcserver.R
@@ -1,8 +1,3 @@
-library(shiny) # for webpage creation
-library(plotly) # for interactive graphs
-library(DT) # for datatables
-library(shinyjs)
-
 selectedData <- reactive({
   df_plot <- goop$combined_df[goop$combined_df$station %in% input$station,]
   event.click.data <- event_data(event = "plotly_click", source = "imgLink")
@@ -11,11 +6,6 @@ selectedData <- reactive({
                           (paste0(df_plot$id,'_',df_plot$station) %in% event.selected.data$key)),]
   return(df_chosen)
 }) 
-
-output$station <- renderUI({
-  num_station <- unique(goop$combined_df$station)
-  radioButtons('station', label = '', num_station)
-})
 
 # Reactive expression for filtered data
 filteredData <- reactive({
@@ -42,12 +32,6 @@ output$end_datetime_input <- renderUI({
 
 # Render the Plotly graph with updated start and end date and time
 output$main_plot <- renderPlotly({
-  # unique_station = unique(goop$combined_df$station)
-  # rainbow_color = rainbow(length(unique_station))
-  # color_mapping <- c()
-  # for(i in seq(unique_station)){
-  #   color_mapping[as.character(i)] <- rainbow_color[i]
-  # }
   p = plot_ly(data = filteredData(), type = 'scatter', mode = 'markers', x = ~Date_Time, y = as.formula(paste0('~', input$variable_choice)), key = ~(paste0(as.character(Date_Time),"_",as.character(station))), color = ~as.character(station), opacity = 0.5, source = "imgLink") |>
     layout(xaxis = list(
       range = c(as.POSIXct(input$start_datetime), as.POSIXct(input$end_datetime)),  # Set the desired range from start date and time to end date and time
