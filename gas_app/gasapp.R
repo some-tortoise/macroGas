ef84d9443d906f430da89adc03d6acc62dd9de7f
remote233 <yyuan2696@gmail.com>
2023-07-07 14:42:54 -0400

    update flagging

--- gas_app/gasapp.R
+++ gas_app/gasapp.R
@@ -12,11 +12,11 @@ library(googledrive)
 library(readr)
 library(shinyBS)
 library(shinythemes)
-
+library(lubridate)
 
 raw_folder <- 'https://drive.google.com/drive/u/0/folders/1hniqK4ouIs3mFC8utRoiRfWgk1Ct-m9k'
 list_of_raw_csv_names = drive_ls(raw_folder)[['name']] #gives us file names from google drive
-
+list_of_raw_csv_id = drive_ls(raw_folder)[['id']]
 ##at this point -- select 1 when prompted to give tidyverse API access to your google drive##
 ##then run the rest of the code 
 
@@ -25,7 +25,8 @@ get_and_clean_data <- function(){
   for(i in 1:length(list_of_raw_csv_names)){ #for each name in however many files we have, do the following:
     name <- list_of_raw_csv_names[i] #gets first name
     station_name <- strsplit(name,'_')[[1]][2] #gets station name
-    file <- drive_get(name)[1,] #if there is a csv by this name, get it.
+    id = as_id(list_of_raw_csv_id[i])
+    file <- drive_get(id)[1,] #if there is a csv by this name, get it.
     drive_download(file, path = name, overwrite = TRUE) # downloads a particular file
     loaded = read.csv(name, header = FALSE) #loads file into r environment
     loaded = loaded[-1] #deleting first column
@@ -34,8 +35,8 @@ get_and_clean_data <- function(){
     loaded <- slice(loaded, -(1:2))
     loaded = loaded %>% #saves following code as loaded
       mutate_at(vars(-Date_Time), as.numeric) %>% #changes every variable but date_time to numeric
-      mutate(Date_Time = as.POSIXct(Date_Time, format = "%m/%d/%y %I:%M:%S %p"),
-             station = station_name) 
+      mutate(Date_Time = parse_date_time(Date_Time, "%m/%d/%y %I:%M:%S %p"),
+              station = station_name) 
     df[[i]] = loaded #makes the ith element of the list equal to the data frame.
   }
   return(df)
@@ -43,6 +44,8 @@ get_and_clean_data <- function(){
 
 clean_dataframe_list <- get_and_clean_data()
 combined_df <- do.call(rbind, clean_dataframe_list)
+combined_df <- melt(combined_df, id.vars = c("Date_Time", "station"), measure.vars = c("DO_conc", "Temp_C"))
+combined_df <- combined_df %>% mutate(Flag = "good", id = row.names(.))
 
 ui <- fluidPage(
   navbarPage('Gas App',
--- gas_app/server/qaqcserver.R
+++ gas_app/server/qaqcserver.R
@@ -2,10 +2,16 @@ selectedData <- reactive({
   df_plot <- goop$combined_df[goop$combined_df$station %in% input$station,]
   event.click.data <- event_data(event = "plotly_click", source = "imgLink")
   event.selected.data <- event_data(event = "plotly_selected", source = "imgLink")
-  df_chosen <- df_plot[((paste0(df_plot$id,'_',df_plot$station) %in% event.click.data$key) | 
-                          (paste0(df_plot$id,'_',df_plot$station) %in% event.selected.data$key)),]
+  df_chosen <- df_plot[((paste0(df_plot$Date_Time,'_',df_plot$station) %in% event.click.data$key) | 
+                          (paste0(df_plot$Date_Time,'_',df_plot$station) %in% event.selected.data$key)),]
+  df_chosen <- df_chosen[df_chosen$variable == input$variable_choice,]
   return(df_chosen)
 }) 
+
+output$variable_c <- renderUI({
+  radioButtons("variable_choice",label = helpText('Select variable to graph'),
+               choices = unique(goop$combined_df$variable))
+})
 
 output$station <- renderUI({
 num_station <- unique(goop$combined_df$station)
@@ -37,26 +43,22 @@ output$end_datetime_input <- renderUI({
 
 # Render the Plotly graph with updated start and end date and time
 output$main_plot <- renderPlotly({
-  p = plot_ly(data = filteredData(), type = 'scatter', mode = 'markers', x = ~Date_Time, y = as.formula(paste0('~', input$variable_choice)), key = ~(paste0(as.character(Date_Time),"_",as.character(station))), color = ~as.character(station), opacity = 0.5, source = "imgLink") |>
+  plot_ly(data = filteredData()[goop$combined_df$variable == input$variable_choice,], type = 'scatter', mode = 'markers', 
+              x = ~Date_Time, y = ~value, key = ~(paste0(as.character(Date_Time),"_",as.character(station))), color = ~as.character(station), opacity = 0.5, source = "imgLink") |>
     layout(xaxis = list(
       range = c(as.POSIXct(input$start_datetime), as.POSIXct(input$end_datetime)),  # Set the desired range from start date and time to end date and time
       type = "date"  # Specify the x-axis type as date
     ), dragmode = 'select') |>
     config(modeBarButtonsToRemove = list("pan2d", "hoverCompareCartesian", "lasso2d", "autoscale", "hoverClosestCartesian")) |>
-    layout(plot_bgcolor='white', xaxis = list(title = 'Date Time'))
-  
-  event_data("plotly_relayout", source = "main_plot")
-  p = event_register(p, 'plotly_relayout')
-  p
+    layout(plot_bgcolor='white', xaxis = list(title = 'Date Time'), yaxis = list(title = input$variable_choice))
 })
 
 output$selected_data_table <- renderDT({
-  datatable(selectedData(), options = list(pageLength = 5, searching = FALSE, lengthChange = FALSE, paging = TRUE, info = FALSE, ordering = FALSE), rownames = FALSE)
+  datatable(selectedData() %>% select(-c(id)), options = list(pageLength = 5, searching = FALSE, lengthChange = FALSE, paging = TRUE, info = FALSE, ordering = FALSE), rownames = FALSE)
 })
 
 observeEvent(input$flag_btn, {
-  flag_name <- paste0(input$variable_choice, "_Flag")
-  goop$combined_df[((goop$combined_df$id %in% selectedData()$id) & (goop$combined_df$station %in% selectedData()$station)), flag_name] <- input$flag_type  # Set the flag
+  goop$combined_df[((goop$combined_df$id %in% selectedData()$id) & (goop$combined_df$station %in% selectedData()$station)), "Flag"] <- input$flag_type  # Set the flag
 })
 
 
