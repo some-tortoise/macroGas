485aa007e4010383e1d26991e33bc3425c380255
kaleyann <sperlingkaley@gmail.com>
2023-07-07 14:57:24 -0400

    changes

--- gas_app/ui/homeUI.R
+++ gas_app/ui/homeUI.R
@@ -1,6 +1,6 @@
 div(class = 'home-panel-container panel-container',
 h1("Welcome to the Gas App"),
 h3("What data would you like to look at?"),
-actionButton("Processed"),
-actionButton("Raw")
+actionButton("processed_button", "Processed Data"),
+actionButton("raw_button", "Raw Data")
 )
