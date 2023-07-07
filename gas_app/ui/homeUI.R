74c115c93f5c48f01545ffbbf63a3d84f75b7e42
kaleyann <sperlingkaley@gmail.com>
2023-07-07 14:54:50 -0400

    ui for home page

--- gas_app/ui/homeUI.R
+++ gas_app/ui/homeUI.R
@@ -1,3 +1,6 @@
 div(class = 'home-panel-container panel-container',
-h1("Hi Nick")
-    )
+h1("Welcome to the Gas App"),
+h3("What data would you like to look at?"),
+actionButton("Processed"),
+actionButton("Raw")
+)
