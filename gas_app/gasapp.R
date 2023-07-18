6f93d49febf7ef00e9739610fbdf133666a09d45
kaleyann <sperlingkaley@gmail.com>
2023-07-18 10:59:13 -0400

    commenting out what must be fixed

--- gas_app/gasapp.R
+++ gas_app/gasapp.R
@@ -16,40 +16,41 @@ library(lubridate)
 library(reshape2)
 library(janitor)
 
-raw_folder <- 'https://drive.google.com/drive/u/0/folders/1hniqK4ouIs3mFC8utRoiRfWgk1Ct-m9k'
-list_of_raw_csv_names = drive_ls(raw_folder)[['name']] #gives us file names from google drive
-list_of_raw_csv_id = drive_ls(raw_folder)[['id']]
-##at this point -- select 1 when prompted to give tidyverse API access to your google drive##
-##then run the rest of the code 
+# raw_folder <- 'https://drive.google.com/drive/u/0/folders/1hniqK4ouIs3mFC8utRoiRfWgk1Ct-m9k'
+# list_of_raw_csv_names = drive_ls(raw_folder)[['name']] #gives us file names from google drive
+# list_of_raw_csv_id = drive_ls(raw_folder)[['id']]
+# ##at this point -- select 1 when prompted to give tidyverse API access to your google drive##
+# ##then run the rest of the code 
+# 
+# get_and_clean_data <- function(){
+#   df = c() #creates an empty vector
+#   for(i in 1:length(list_of_raw_csv_names)){ #for each name in however many files we have, do the following:
+#     name <- list_of_raw_csv_names[i] #gets first name
+#     station_name <- strsplit(name,'_')[[1]][2] #gets station name
+#     id = as_id(list_of_raw_csv_id[i])
+#     file <- drive_get(id)[1,] #if there is a csv by this name, get it.
+#     drive_download(file, path = name, overwrite = TRUE) # downloads a particular file
+#     loaded = read.csv(name, header = FALSE) #loads file into r environment
+#     loaded = loaded[-1] #deleting first column
+#     loaded = loaded[1:3] #keeping first 3 columns
+#     colnames(loaded) <- c('Date_Time', 'DO_conc', 'Temp_C') #naming columns
+#     loaded <- slice(loaded, -(1:2))
+#     loaded = loaded %>% #saves following code as loaded
+#       mutate_at(vars(-Date_Time), as.numeric) %>% #changes every variable but date_time to numeric
+#       mutate(Date_Time = parse_date_time(Date_Time, "%m/%d/%y %I:%M:%S %p"),
+#               station = station_name) 
+#     df[[i]] = loaded #makes the ith element of the list equal to the data frame.
+#   }
+#   return(df)
+# }
 
-get_and_clean_data <- function(){
-  df = c() #creates an empty vector
-  for(i in 1:length(list_of_raw_csv_names)){ #for each name in however many files we have, do the following:
-    name <- list_of_raw_csv_names[i] #gets first name
-    station_name <- strsplit(name,'_')[[1]][2] #gets station name
-    id = as_id(list_of_raw_csv_id[i])
-    file <- drive_get(id)[1,] #if there is a csv by this name, get it.
-    drive_download(file, path = name, overwrite = TRUE) # downloads a particular file
-    loaded = read.csv(name, header = FALSE) #loads file into r environment
-    loaded = loaded[-1] #deleting first column
-    loaded = loaded[1:3] #keeping first 3 columns
-    colnames(loaded) <- c('Date_Time', 'DO_conc', 'Temp_C') #naming columns
-    loaded <- slice(loaded, -(1:2))
-    loaded = loaded %>% #saves following code as loaded
-      mutate_at(vars(-Date_Time), as.numeric) %>% #changes every variable but date_time to numeric
-      mutate(Date_Time = parse_date_time(Date_Time, "%m/%d/%y %I:%M:%S %p"),
-              station = station_name) 
-    df[[i]] = loaded #makes the ith element of the list equal to the data frame.
-  }
-  return(df)
-}
-
-clean_dataframe_list <- get_and_clean_data()
+# clean_dataframe_list <- get_and_clean_data()
 combined_df <- do.call(rbind, clean_dataframe_list)
-combined_df <- melt(combined_df, id.vars = c("Date_Time", "station"), measure.vars = c("DO_conc", "Temp_C"))
+combined_df <- melt(combined_df, id.vars = c("Date_Time", "station", "site")#, measure.vars = c("DO_conc", "Temp_C"))
 combined_df <- combined_df |>
   rename(Variable = variable,
          Station = station,
+         Site = site,
          Value = value)
 combined_df <- combined_df %>% mutate(Flag = "NA", id = row.names(.))
 
