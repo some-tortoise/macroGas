3a05682908f466a5450f4d8336edeacc047c3f8e
annaspitzer <anna.spitzer@duke.edu>
2023-07-18 10:47:03 -0400

    functions !!! R genius !!!!

--- gas_app/NHRmath.R
+++ gas_app/NHRmath.R
@@ -4,94 +4,112 @@ library(tidyr)
 library(ggplot2)
 library(pracma) #integrals
 
-#calculate fraction of observations that are 
-#hypoxic at night / fractions hypoxic during the day
-
-
-#axes of the graph are x = DO (% sat) and y = probability density
-#one line is probability 
 path <- "/Users/annacspitzer/Desktop/erwin_downstream_DO.csv" 
 exampledata <- read.csv(path)
-
-#deal with the datetime column
 exampledata$Date_Time <- mdy_hms(exampledata$Date_Time)
 
-#extract hour using lubridate and move the column to after Date_Time
-exampledata$Hour <- lubridate::hour(exampledata$Date_Time)
-
-# assuming sun sets at 8PM rises at 6AM
-# light data fram, 6AM to 8PM
-# light_df <- filter(exampledata, Hour %in% c(6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
-
-# dark data fram, 8PM to 6AM
-# dark_df <- filter(exampledata, Hour %in% c(21, 22, 23, 0, 1, 2, 3, 4, 5))
-
-#hypoxia threshold
+#inputs
+start_date_time <- "2023-06-15 00:00:00"
+end_date_time <- "2023-06-17 11:59:00"
+sunrise <- "06:00:00"
+sunset <- "20:00:00"
 h <- 4
+data <- exampledata
 
-##### june 15th through 17th #####
-june15 <- exampledata[1398:1686, ]
-june15_dark <- filter(june15, Hour %in% c(21, 22, 23, 0, 1, 2, 3, 4, 5))
-june15_light <- filter(june15, Hour %in% c(6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20))
-
-#light probability density
-n_light <- nrow(june15_light) #gets number of  observations
-hypoxic_n_light <- sum(june15_light$DO_conc < h, na.rm = TRUE) #gets number of hypoxic observations
-light_prob_dens <- (hypoxic_n_light/n_light)
-print("Light probability density")
-print(light_prob_dens)
-
-#dark probability density
-n_dark <- nrow(june15_dark)
-hypoxic_n_dark <- sum(june15_dark$DO_conc < h, na.rm = TRUE)
-dark_prob_dens <- (hypoxic_n_dark/n_dark)
-print("Night probability density")
-print(dark_prob_dens)
-
-#Night hypoxia ratio
-nhr <- (dark_prob_dens/light_prob_dens)
-print("Night Hypoxia Ratio")
-print(nhr)
+## can run all these in order and will get you new light/dark dataframes and calculate all the probabilities ##
+clean_DO_data(data, start_date_time, end_date_time)
+get_dark_light_df(clean_data, sunrise, sunset)
+light_prob_fxn(light_df, h)
+dark_prob_fxn(dark_df, h)
+night_hyp_ratio(dark_prob_dens, light_prob_dens)
 
 
-##### PROBABILITY DENSITY OF BOTH #####
-cleaned_data <- june15[!is.na(june15$DO_conc), ]
-kde_all <- density(cleaned_data$DO_conc)
-plot_all <- data.frame(DO = kde_all$x, Density = kde_all$y)
-ggplot(plot_all, aes(x = DO, y = Density)) +
-  geom_line() +
-  labs(x = "Dissolved Oxygen (mg/L)", y = "Probability Density") +
-  ggtitle("Probability Density of Dissolved Oxygen (BOTH)")
+## functions ##
+clean_DO_data <- function(data, start_date_time, end_date_time) {
+  clean_data <- data %>% 
+    mutate(Date_Time = ymd_hms(Date_Time)) %>%
+    filter(Date_Time >= ymd_hms(start_date_time) & Date_Time <= ymd_hms(end_date_time)) %>%
+    mutate(Hour = hour(Date_Time)) %>%
+    mutate(Minute = minute(Date_Time))
+  
+  clean_data <<- clean_data
+  return(clean_data)
+  
+  }
+get_dark_light_df <- function(clean_data, sunrise, sunset) {
 
+#Fix formatting of dates and add an hour and minute column 
+  sunrise_time <- hms(sunrise)
+  sunset_time <- hms(sunset)
+  
+  light_df <<- clean_data %>%
+    filter(Hour > hour(sunrise_time) | (Hour == hour(sunrise_time) & Minute >= minute(sunrise_time))) %>%
+    filter(Hour < hour(sunset_time) | (Hour == hour(sunset_time) & Minute <= minute(sunset_time)))
+  
+  dark_df <<- clean_data %>%
+    filter(Hour > hour(sunset_time) | (Hour == hour(sunset_time) & Minute >= minute(sunset_time)) | (Hour < hour(sunrise_time) | (Hour == hour(sunrise_time) & Minute <= minute(sunrise_time))))
+  
+}
+light_prob_fxn <- function(light_df, h) {
+  n_light <- nrow(light_df)
+  hypoxic_n_light <- sum(light_df$DO_conc < h, na.rm = TRUE) #gets number of hypoxic observations
+  light_prob_dens <<- (hypoxic_n_light/n_light)
+  print("Light probability density")
+  print(light_prob_dens)
+}
+dark_prob_fxn <- function(dark_df, h) {
+  n_dark <- nrow(dark_df)
+  hypoxic_n_dark <- sum(dark_df$DO_conc < h, na.rm = TRUE)
+  dark_prob_dens <<- (hypoxic_n_dark/n_dark)
+  print("Night probability density")
+  print(dark_prob_dens)
+}
+night_hyp_ratio <- function(dark_prob_dens, light_prob_dens) {
+  nhr <<- (dark_prob_dens/light_prob_dens)
+  
+  print("Night Hypoxia Ratio")
+  print(nhr)
+  return(nhr)
+}
 
-##### PROBABILITY DENSITY OF NIGHT ######
-kde_dark <- density(june15_dark$DO_conc)
-plot_dark <- data.frame(DO = kde_dark$x, Density = kde_dark$y)
-ggplot(plot_dark, aes(x = DO, y = Density)) +
-  geom_line() +
-  geom_vline(xintercept = h, color = "red") +  # Add a vertical line at x = 8
-  geom_ribbon(data = subset(plot_dark, DO <= h), aes(x = DO, ymin = 0, ymax = Density),
-              fill = "darkblue", alpha = 0.3) +  # Shade to the left of the line
-  labs(x = "Dissolved Oxygen (mg/L)", y = "Probability Density") +
-  ggtitle("Erwin Downstream June 15-17: Dark")
+## GRAPH AND INTEGRAL FUNCTION ##
+plot_density <- function(df) {
+  kde <- density(df$DO_conc)
+  plot <- data.frame(DO = kde$x, Density = kde$y)
+  ggplot(plot, aes(x = DO, y = Density)) +
+    geom_line() +
+    geom_vline(xintercept = h, color = "red") +
+    geom_ribbon(data = subset(plot, DO <= h), aes(x = DO, ymin = 0, ymax = Density),
+                fill = "darkblue", alpha = 0.3) +
+    labs(x = "Dissolved Oxygen (mg/L)", y = "Probability Density") +
+    ggtitle("Erwin Downstream June 15-17")
+  
+  subset_df <- subset(plot, DO >= min(DO) & DO <= h)
+  integral <<- trapz(subset_df$DO, subset_df$Density)
+  
+}
 
-subset_df_dark <- subset(plot_dark, DO >= min(DO) & DO <= h)
-integral_dark <- trapz(subset_df_dark$DO, subset_df_dark$Density)
+## define your dataframe (either light, dark, or whole) and then run this function
+# input example: df <- light_df
+plot_density(df)
 
-##### PROB DENSITY OF DAY #####
-cleaned_light_df <- june15_light[!is.na(june15_light$DO_conc), ]
-kde_light <- density(cleaned_light_df$DO_conc)
-plot_light <- data.frame(DO = kde_light$x, Density = kde_light$y)
-ggplot(plot_light, aes(x = DO, y = Density)) +
-  geom_line() +
-  geom_vline(xintercept = h, color = "red") +  
-  geom_ribbon(data = subset(plot_light, DO <= h), aes(x = DO, ymin = 0, ymax = Density),
-              fill = "red", alpha = 0.3) +  # Shade to the left of the line
-  labs(x = "Dissolved Oxygen (mg/L)", y = "Probability Density") +
-  ggtitle("Erwin Downstream June 15-17: Light")
-
-subset_df_light <- subset(plot_light, DO >= min(DO) & DO <= h)
-integral_light <- trapz(subset_df_light$DO, subset_df_light$Density)
-
+##### PROBABILITY DENSITY OF NIGHT PLOT FXN ######
+plot_density <- function(df) {
+  kde <- density(df$DO_conc)
+  plot <- data.frame(DO = kde$x, Density = kde$y)
+  ggplot(plot, aes(x = DO, y = Density)) +
+    geom_line() +
+    geom_vline(xintercept = h, color = "red") +
+    geom_ribbon(data = subset(plot, DO <= h), aes(x = DO, ymin = 0, ymax = Density),
+                fill = "darkblue", alpha = 0.3) +
+    labs(x = "Dissolved Oxygen (mg/L)", y = "Probability Density") +
+    ggtitle("Erwin Downstream June 15-17")
+  
+  subset_df <- subset(plot, DO >= min(DO) & DO <= h)
+  integral_prob <<- trapz(subset_df$DO, subset_df$Density)
+  
+  
+}
+ 
 
 
